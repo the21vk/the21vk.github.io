4aa18b2b5cabf8fba2c2c5888ad4665cc1d11a23
Vinay Kumar <vinay.kumar@icts.res.in>
2024-12-23 18:01:27 +0530

    colors

--- Phantom/assets/js/main.js
+++ Phantom/assets/js/main.js
@@ -183,3 +183,23 @@
 			});
 
 })(jQuery);
+
+let index = 0; // Current slide
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(n) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === n ? "block" : "none";
+  });
+}
+
+// Next/previous controls
+function changeSlides(n) {
+  index += n;
+  if (index >= slides.length) index = 0;
+  if (index < 0) index = slides.length - 1;
+  showSlide(index);
+}
+
+// Initial display
+showSlide(index);
